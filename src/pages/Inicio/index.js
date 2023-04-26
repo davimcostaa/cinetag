83ce23cb39cd57ca7cb85b66d79a3faf6ab174d0
Davi Costa <davim.costa@outlook.com>
2023-04-26 18:01:00 -0300

    criação dos componentes

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,8 @@
 import Banner from 'components/Banner'
 import Cabecalho from 'components/Cabecalho'
+import Card from 'components/Card'
 import Rodape from 'components/Rodape'
+import Titulo from 'components/Titulo'
 import React from 'react'
 
 const Inicio = () => {
@@ -8,6 +10,10 @@ const Inicio = () => {
     <>
       <Cabecalho />
         <Banner imagem="home" />
+        <Titulo>
+          <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+        </Titulo>
+        <Card id='1' titulo='Gato Bonifácio' capa='https://cdn2.thecatapi.com/images/AkdUb5Ddk.jpg' />
       <Rodape />
     </>
 
